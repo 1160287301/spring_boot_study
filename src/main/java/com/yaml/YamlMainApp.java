a9ceba0edcf4b4030cecbebef6cfac461d2ae012
huangpeng <huangpeng@sensorsdata.cn>
2019-11-15 16:18:01 +0800

    第三次提交

---
+++ src/main/java/com/yaml/YamlMainApp.java
@@ -0,0 +1,12 @@
+package com.yaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlMainApp {
+    public static void main(String[] args) {
+        // Spring应用启动起来
+        SpringApplication.run(YamlMainApp.class, args);
+    }
+}
