13c02d8012b079c647aeb0682387554426b4a469
huangpeng <huangpeng@sensorsdata.cn>
2020-11-25 18:34:18 +0800

    commit

--- src/main/java/com/hello/controller/HelloController.java
+++ src/main/java/com/hello/controller/HelloController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//@ResponseBody  // 这个类的所有方法返回的数据直接写给浏览器,如果返回的是对象则转为 json 的数据
 @Controller
+//@RestController // 就是 controller 和 responsebody 的组合注解
 public class HelloController {
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World!!";
     }
+
+
 }
--- src/main/java/com/springboot/bean/Person.java
+++ src/main/java/com/springboot/bean/Person.java
@@ -2,7 +2,7 @@ package com.springboot.bean;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -12,28 +12,27 @@ import java.util.Map;
 
 /**
  * 将配置文件中配置的每一个属性的值，映射到这个组件中
- * @ConfigurationProperties：告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定；
- *      prefix = "person"：配置文件中哪个下面的所有属性进行一一映射
  *
+ * @ConfigurationProperties：告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定； prefix = "person"：配置文件中哪个下面的所有属性进行一一映射
+ * <p>
  * 只有这个组件是容器中的组件，才能容器提供的@ConfigurationProperties功能；
- *  @ConfigurationProperties(prefix = "person")默认从全局配置文件中获取值；
- *
+ * @ConfigurationProperties(prefix = "person")默认从全局配置文件中获取值；
  */
 
 @Component
-//@ConfigurationProperties(prefix = "person")
-@PropertySource("classpath:person.properties")
+@ConfigurationProperties(prefix = "person")
+//@PropertySource("classpath:person.properties")
 //@Validated
 public class Person {
 
     /**
      * <bean class="Person">
-     *      <property name="lastName" value="字面量/${key}从环境变量、配置文件中获取值/#{SpEL}"></property>
+     * <property name="lastName" value="字面量/${key}从环境变量、配置文件中获取值/#{SpEL}"></property>
      * <bean/>
      */
 
-   //lastName必须是邮箱格式
-   // @Email
+    //lastName必须是邮箱格式
+    // @Email
     @Value("${person.last-name}")
     private String lastName;
     @Value("${person.age}")
@@ -43,7 +42,7 @@ public class Person {
 
     private Date birth;
     //@Value("${person.maps}")
-    private Map<String,Object> maps;
+    private Map<String, Object> maps;
     private List<Object> lists;
     private Dog dog;
 
