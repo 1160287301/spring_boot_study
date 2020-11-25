80c6e482dffed9bca2a50d9f89c2eb5b6fc948ff
huangpeng <huangpeng@sensorsdata.cn>
2020-11-25 19:01:09 +0800

    commit

--- src/main/java/com/springboot/controller/HelloController.java
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -1,14 +1,26 @@
 package com.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Component
 public class HelloController {
+    @Value("${person.last-name}")
+    private String name;
+
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World!!";
+    }
+
+    @ResponseBody
+    @RequestMapping("/sayHello")
+    public String sayHello() {
+        return "Hello World " + name;
     }
 }
